f078bc9e8618e2af040fdef62c91dac901260a75
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-11 21:39:27 -0800

    Update introToRuby.rb

---
+++ introToRuby.rb
@@ -0,0 +1,15 @@
+#code snippets from the reading
+puts "hello" #prints hello. puts is a method
+
+3.times do 
+    print "hello, world"
+end
+#prints hello, world x3
+
+#Primitive Data Types: strings, numbers, booleans, nil
+
+#modulo operations
+5 % 2
+5.0 % 2.0
+4 % 2
+4.0 % 2.0
