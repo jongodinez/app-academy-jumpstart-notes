4f9ef5babd0f1c7e199d0c1bf6785f397b7b2906
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-15 01:28:36 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -131,3 +131,13 @@ end
 
 puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
 puts sum_nums(5) # => 15
+
+#Write a method factorial(num) that takes in a number num and returns the product of all numbers from 1 up to and including num.
+def factorial(num)
+    product = 1
+    i = 1
+    while i <= num
+        product *= i # product = product * i
+        i += 1
+    end
+end
