73ce0e76de7d0269b0c3721e0b8191a9a0d3dc19
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-15 01:18:51 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -117,3 +117,17 @@ end
 puts count_vowels("bootcamp")  # => 3
 puts count_vowels("apple")     # => 2
 puts count_vowels("pizza")     # => 2
+
+#Write a method sum_nums(max) that takes in a number max and returns the sum of all numbers from 1 up to and including max.
+def sum_nums(max)
+    sum = 0
+    i = 1
+    while i <= max
+        sum += i #sum increments by the value of i
+        i += 1
+  end
+return sum
+end
+
+puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
+puts sum_nums(5) # => 15
