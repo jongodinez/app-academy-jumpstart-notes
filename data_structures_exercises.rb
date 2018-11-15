65fa3c7d748e45578da8999eaea47ee3d0dedb5f
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-15 02:55:25 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -159,3 +159,16 @@ puts reverse("programming")  # => "gnimmargorp"
 puts reverse("bootcamp")     # => "pmactoob"
 
 #Write a method is_palindrome(word) that takes in a string word and returns the true if the word is a palindrome, false otherwise. A palindrome is a word that is spelled the same forwards and backwards.
+def is_palindrome(word)
+    palindrome = ""
+    i = 0
+    while i < word.length
+        palindrome = word[i] + palindrome
+        i += 1
+    end
+    return palindrome == word
+end
+
+puts is_palindrome("racecar")  # => true
+puts is_palindrome("kayak")    # => true
+puts is_palindrome("bootcamp") # => false
