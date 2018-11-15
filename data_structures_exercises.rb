8a3c81f58b81782100ec95036d70e8f4b0839386
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-15 00:57:09 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -99,3 +99,21 @@ def count_a(word)
     end
   return counter
 end
+
+#Write a method, count_vowels(word), that takes in a string word and returns the number of vowels in the word. Vowels are the letters a, e, i, o, u.
+def count_vowels(word)
+	counter = 0
+  	i = 0
+  	while i < word.length
+     vowel = word[i]
+      if vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u"
+        counter += 1
+      end
+      i += 1
+    end
+  return counter
+end
+
+puts count_vowels("bootcamp")  # => 3
+puts count_vowels("apple")     # => 2
+puts count_vowels("pizza")     # => 2
