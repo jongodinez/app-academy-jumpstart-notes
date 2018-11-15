7cb315060bbf578331731343daa8a639c50f80ea
jongodinez <jonathanjgodinez@gmail.com>
2018-11-14 18:38:14 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -20,3 +20,44 @@ end
 
 print_nums #decrements 10-1
 
+def print_nums1
+    i = 0
+    while i <= 100
+        puts i
+        i += 1
+    end
+end
+
+print_nums1
+
+def printNums
+    i = 1
+    while i <= 5
+        puts i
+        i -= 1
+    end
+end
+printNums #decrements forever
+
+def printMinMax(min, max)
+    i = min
+    while i <= max
+        puts i
+        i += 1
+    end
+end
+
+printMinMax(1, 100)
+printMinMax(5, 500)
+
+#introduction of the step
+
+def printStep(min,max, step)
+    i = min
+    while i <= max
+        puts i
+        i += step
+    end
+end
+
+printStep(0, 4, 1) # code will iterate the amount of times specified in the step
