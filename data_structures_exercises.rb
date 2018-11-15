02ddbf6fa7521f81ef3146db7df9e79f1354b2ae
jongodinez <jonathanjgodinez@gmail.com>
2018-11-14 18:15:59 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -1,1 +1,12 @@
-placeholder
+#Loops
+def say_hello
+    counter = 1
+    while counter <= 5
+        puts "hello"
+        counter += 1
+    end
+end
+
+say_hello #increments "hello" 5 times
+
+#de
