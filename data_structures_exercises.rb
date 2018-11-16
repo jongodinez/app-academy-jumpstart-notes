cb2756b302f45eb98ff3bfa69e3558fe49a53596
jongodinez <jonathanjgodinez@gmail.com>
2018-11-16 10:56:18 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -200,3 +200,14 @@ def yell(words)
     return new_arr
 end
 
+
+while i < numbers. length
+    new_arr<< numbers[i] * i
+      i += 1
+end
+return new_arr
+end
+
+print element_times_index([4, 7, 6, 5])       # => [0, 7, 12, 15]
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) # => [0, 1, 2, 3, 4, 5]
