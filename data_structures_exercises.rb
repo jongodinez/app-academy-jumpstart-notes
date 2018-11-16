eec295708b8f4fb6a59b324f95066570743bd588
jongodinez <jonathanjgodinez@gmail.com>
2018-11-16 11:34:07 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -235,3 +235,24 @@ def even_nums(max)
     end
     print new_arr
 end
+
+#Write a method range(min, max) that takes in two numbers min and max. The function should return an array containing all numbers from min to max inclusive.
+    #my solution
+def range(min, max)
+	print (min..max).to_a
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
+
+    #their solution
+def range(min, max)
+    new_arr = []
+    i = min
+        while i <= max
+        new_arr << i
+        i += 1
+        end
+        return new_arr
+    end
