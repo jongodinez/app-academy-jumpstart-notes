8d742dcab2e805b07fe8f26af48db83e0a69deee
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-11-16 14:35:41 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -256,3 +256,21 @@ def range(min, max)
         end
         return new_arr
     end
+
+#Write a method select_long_words(words) that takes in an array of words and returns a new array containing all of the words of the original array that are longer than 4 characters.
+
+def select_long_words(words)
+	new_arr = []
+  	i = 0
+  	while i < words.length
+      if words[i].length > 4
+		new_arr << words[i]
+      end
+      i += 1
+    end
+  	return new_arr
+end
+
+print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
+puts
+print select_long_words(["keep", "coding"])                               # => ["coding"]
