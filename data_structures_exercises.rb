fe2e8a7f51861ede7e62f8b95e876080ce63ec41
jongodinez <jonathanjgodinez@gmail.com>
2018-11-17 13:25:30 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -379,6 +379,22 @@ puts
 print fizz_buzz(15) # => [4, 6, 8]
 
 #Write a method reverse_range(min, max) that takes in two numbers min and max. The function should return an array containing all numbers from min to max in reverse order. The min and max should be excluded from the array
+def reverse_range(min, max)
+	new_arr = []
+  	i = max - 1
+  	while i > min
+		new_arr << i
+      	i -= 1
+    end
+  	return new_arr
+end
+      	
+
+print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
+puts
+print reverse_range(1, 7)   # => [6, 5, 4, 3, 2]
+
+
 
 
   
