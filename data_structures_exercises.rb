368f705ac9968da09e3ccd60cf4c797fd3275a9d
jongodinez <jonathanjgodinez@gmail.com>
2018-11-21 23:14:28 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -335,15 +335,15 @@ def factors_of(num)
     return new_arr
 end
   
-  print factors_of(3)   # => [1, 3]
-  puts
-  print factors_of(4)   # => [1, 2, 4]
-  puts
-  print factors_of(8)   # => [1, 2, 4, 8]
-  puts
-  print factors_of(9)   # => [1, 3, 9]
-  puts
-  print factors_of(16)  # => [1, 2, 4, 8, 16]
+print factors_of(3)   # => [1, 3]
+puts
+print factors_of(4)   # => [1, 2, 4]
+puts
+print factors_of(8)   # => [1, 2, 4, 8]
+puts
+print factors_of(9)   # => [1, 3, 9]
+puts
+print factors_of(16)  # => [1, 2, 4, 8, 16]
 
 #Write a method sum_elements(arr1, arr2) that takes in two arrays. The method should return a new array containing the results of adding together corresponding elements of the original arrays. You can assume the arrays have the same length.
 
