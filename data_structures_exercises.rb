91bdda497b7e4497c024a9ea1823d09b03c87873
jongodinez <jonathanjgodinez@gmail.com>
2018-11-21 23:13:51 -0800

    Update data_structures_exercises.rb

--- data_structures_exercises.rb
+++ data_structures_exercises.rb
@@ -405,7 +405,9 @@ def first_half(array)
 end
 
 print first_half(["Brian", "Abby", "David", "Ommi"]) # => ["Brian", "Abby"]
+
 puts
+
 print first_half(["a", "b", "c", "d", "e"])          # => ["a", "b", "c"]
 
 
